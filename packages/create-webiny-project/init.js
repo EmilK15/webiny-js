0a8620f6008654d10876f2ca4e10e6b967c41d17
Emil Kais <emil.kais94@gmail.com>
2020-04-21 09:41:53 -0400

    Added tar-pack library. Added previously undefined function definition.

--- packages/create-webiny-project/init.js
+++ packages/create-webiny-project/init.js
@@ -1,105 +1,105 @@
-'use strict';
+"use strict";
 
-process.on('unhandledRejection', err => {
-    throw err;
+process.on("unhandledRejection", err => {
+	throw err;
 });
 
-const chalk = require('chalk');
-const execa = require('execa');
-const fs = require('fs-extra');
-const path = require('path');
-const os = require('os');
+const chalk = require("chalk");
+const execa = require("execa");
+const fs = require("fs-extra");
+const path = require("path");
+const os = require("os");
 
 module.exports = function(root, appName, templateName) {
-    const appPackage = require(path.join(root, 'package.json'));
+	const appPackage = require(path.join(root, "package.json"));
 
-    if(!templateName) {
-        console.log('Please provide a template.');
-        return;
-    }
+	if(!templateName) {
+		console.log("Please provide a template.");
+		return;
+	}
 
-    const templatePath = path.dirname(
-        require.resolve(`${templateName}/package.json`, { paths: [root] })
-    );
+	const templatePath = path.dirname(
+		require.resolve(`${templateName}/package.json`, { paths: [root] })
+	);
 
-    let templateJson = {};
-    const templateJsonPath = path.join(templatePath, 'package.json');
+	let templateJson = {};
+	const templateJsonPath = path.join(templatePath, "package.json");
 
-    if (fs.existsSync(templateJsonPath)) {
-        templateJson = require(templateJsonPath);
-    }
+	if (fs.existsSync(templateJsonPath)) {
+		templateJson = require(templateJsonPath);
+	}
 
-    // Keys to ignore in templatePackage
-    const templatePackageBlacklist = [
-        'name',
-        'version',
-        'description',
-        'keywords',
-        'bugs',
-        'license',
-        'author',
-        'contributors',
-        'files',
-        'browser',
-        'bin',
-        'man',
-        'directories',
-        'repository',
-        'bundledDependencies',
-        'optionalDependencies',
-        'engineStrict',
-        'os',
-        'cpu',
-        'preferGlobal',
-        'private',
-        'publishConfig',
-    ];
+	// Keys to ignore in templatePackage
+	const templatePackageBlacklist = [
+		"name",
+		"version",
+		"description",
+		"keywords",
+		"bugs",
+		"license",
+		"author",
+		"contributors",
+		"files",
+		"browser",
+		"bin",
+		"man",
+		"directories",
+		"repository",
+		"bundledDependencies",
+		"optionalDependencies",
+		"engineStrict",
+		"os",
+		"cpu",
+		"preferGlobal",
+		"private",
+		"publishConfig",
+	];
 
-    const templatePackageToReplace = Object.keys(templateJson).filter(key => {
-        return !templatePackageBlacklist.includes(key);
-    });
+	const templatePackageToReplace = Object.keys(templateJson).filter(key => {
+		return !templatePackageBlacklist.includes(key);
+	});
 
-    // Add templatePackage keys/values to appPackage, replacing existing entries
-    templatePackageToReplace.forEach(key => {
-        appPackage[key] = templateJson[key];
-    });
+	// Add templatePackage keys/values to appPackage, replacing existing entries
+	templatePackageToReplace.forEach(key => {
+		appPackage[key] = templateJson[key];
+	});
 
-    fs.writeFileSync(
-        path.join(root, 'package.json'),
-        JSON.stringify(appPackage, null, 2) + os.EOL
-    );
+	fs.writeFileSync(
+		path.join(root, "package.json"),
+		JSON.stringify(appPackage, null, 2) + os.EOL
+	);
 
-    // Copy the files for the user
-    const templateDir = path.join(templatePath, 'template');
-    if (fs.existsSync(templateDir)) {
-        fs.copySync(templateDir, root);
-      } else {
-        console.error(`Could not locate supplied template: ${chalk.green(templateDir)}`);
-        return;
-    }
+	// Copy the files for the user
+	const templateDir = path.join(templatePath, "template");
+	if (fs.existsSync(templateDir)) {
+		fs.copySync(templateDir, root);
+	} else {
+		console.error(`Could not locate supplied template: ${chalk.green(templateDir)}`);
+		return;
+	}
 
-    //initialize git repo
-    try {
-        execa.sync('git',  ['--version']);
-        execa.sync('git', ['init']);
-        console.log('\nInitialized a git repository.');
-        fs.writeFileSync(path.join(root, '.gitignore'), 'node_modules/');
-    } catch(err) {
-        console.warn('Git repo not initialized', err);
-    }
+	//initialize git repo
+	try {
+		execa.sync("git",  ["--version"]);
+		execa.sync("git", ["init"]);
+		console.log("\nInitialized a git repository.");
+		fs.writeFileSync(path.join(root, ".gitignore"), "node_modules/");
+	} catch(err) {
+		console.warn("Git repo not initialized", err);
+	}
 
-    // Remove template from dependencies
-    try {
-        execa.sync('yarn', ['remove', templateName], {
-            cwd: root
-        });
-    } catch (err) {
-        console.error('Unable to remove ' + templateName);
-    }
+	// Remove template from dependencies
+	try {
+		execa.sync("yarn", ["remove", templateName], {
+			cwd: root
+		});
+	} catch (err) {
+		console.error("Unable to remove " + templateName);
+	}
 
-    console.log(`Success! Created ${appName} at ${root}`);
-    console.log('Inside that directory, you can run several commands:\n');
-    console.log('We suggest that you begin by typing:\n');
-    console.log(chalk.cyan('  cd'), appName);
-    console.log('Happy hacking!');
-}
+	console.log(`Success! Created ${appName} at ${root}`);
+	console.log("Inside that directory, you can run several commands:\n");
+	console.log("We suggest that you begin by typing:\n");
+	console.log(chalk.cyan("  cd"), appName);
+	console.log("Happy hacking!");
+};
